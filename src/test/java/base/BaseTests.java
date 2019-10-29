3c34b0bfae73637d78b11055a1ee8b090670c7fe
tetianakharyna <tetiana.kharyna@gen.tech>
2019-10-29 14:35:00 +0200

    fixed last version of base test

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import pages.IndexSePage;
 import pages.LoginPageWithEmail;
 import pages.MainDashboardPage;
@@ -13,7 +15,7 @@ public class BaseTests {
     private WebDriver driver; //create a webdriver object
     protected IndexSePage indexSePage;
 
-
+@BeforeClass
     public void setUp() {
         System.setProperty ("webdriver.chrome.driver", "resourses/chromedriver");
         //lets intantiate chrome driver ogject
@@ -27,19 +29,17 @@ public class BaseTests {
         MainDashboardPage mainDashboardPage = loginPageWithEmail.clickLoginPrimaryButton();
         mainDashboardPage.getAlertText();
         System.out.println(driver.getTitle());
-
-        // provide a handle
-
-//        WebElement loginButton = driver.findElement(By.linkText("Logga in"));
-//        loginButton.click();
-//        System.out.println(driver.getTitle());
     }
 
     public static void main (String [] args){
         BaseTests test = new BaseTests();
         test.setUp();
     }
-    ///////
-    ////
 
+    @AfterClass
+    public void tearDown (){
+        driver.quit();
+    }
+
+}
 }
